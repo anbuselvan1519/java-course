e3c5b00fbb45d9c14f7a04516f398392b16fb9fe
anbuselvan1519 <anbuselvanSK2006@gmail.com>
2025-04-29 10:56:58 +0530

    "Basics topics of java"

--- ReverseStringRecurse.java
+++ ReverseStringRecurse.java
@@ -1,5 +1,13 @@
-public class ReverseStringRecurse{
+public class ReverseStringRecurse {
     public static void main(String[] args) {
-        
+        String s = "hello";
+        System.out.println(rrs(s, s.length() - 1));
+    }
+
+    public static String rrs(String s, int n) {
+        if (n < 0) { 
+            return "";
+        }
+        return s.charAt(n) + rrs(s, n - 1); 
     }
 }
